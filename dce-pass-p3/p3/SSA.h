7e6672532bfa841e9c0a9661f37ef1ef6b2ccb24
felipear <feliperosasd@gmail.com>
2015-05-27 16:07:36 -0300

    Tinha colocado o .cpp no .h. Corrigindo

--- dce-pass-p3/p3/SSA.h
+++ dce-pass-p3/p3/SSA.h
@@ -1,20 +1,55 @@
-#include "SSA.h"
 
-using namespace std;
+#include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/InstIterator.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/IR/User.h"
+#include "llvm/IR/Instructions.h"
+#include <set>
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Analysis/CFG.h"
+#include "llvm/ADT/BitVector.h"
+#include "llvm/IR/ValueMap.h"
+
+
+using namespace llvm; 
+
+namespace llvm {
+
+class SSA: public FunctionPass {
+private:
+
+    //DenseMap<const Instruction*, LivenessInfo> iLivenessMap;
+    //DenseMap<const BasicBlock*, LivenessInfo> bbLivenessMap;
+
+    //DenseMap<const Instruction*, int> instMap;
+
+
+public:
+	static char ID; 
+	SSA() : FunctionPass(ID) {}
+
+	virtual bool runOnFunction(Function &F);
+    //void computeBBDefUse(Function &F);
+   // void computeBBInOut(Function &F);
+
+    //void computeIInOut(Function &F);
+
+   // bool isLiveOut(Instruction *I, Value *V);
+    //void addToMap(Function &F);
+
+	/*virtual void getAnalysisUsage(AnalysisUsage &AU) const{
+		AU.setPreservesAll();
+	}*/
 
 
 
-bool SSA::runOnFunction(Function &F){
-	//Preciso encontrar uma função que pega o ultimo bloco.
-	//for (Function::reverse_iterator i = F.rbegin(); i != F.rend(); ++i ) { 
-		//
-	//} 
-}
-
-
-
-
-
-char SSA::ID = 0;
-
-RegisterPass<SSA> X("ssa", "DCE para SSA", false, false);
+};
+	
+	
+} 
