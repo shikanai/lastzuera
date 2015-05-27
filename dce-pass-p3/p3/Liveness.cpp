70cc9b4b0828a17221a19d5b40bcc09ec4e252ca
shikanai <femassunari@gmail.com>
2015-05-27 17:06:00 -0300

    Update Liveness.cpp

--- dce-pass-p3/p3/Liveness.cpp
+++ dce-pass-p3/p3/Liveness.cpp
@@ -11,23 +11,35 @@ void print_elem(const Value* i) {
 }
 
 bool Liveness::isLiveOut(Instruction *I, Value *V){
+	errs() << "bla1\n";
     return false;
 }
 
+//bb -> basic block
 void Liveness::computeBBDefUse(Function &F){
+	errs() << "bla2\n";
 }
 
 void Liveness::computeBBInOut(Function &F){
+	errs() << "bla3\n";
 }
 
 void Liveness::computeIInOut(Function &F) {
+	errs() << "bla4\n";
+	for (Function::iterator b = F.begin(), e = F.end(); b != e; ++b) {
+        BasicBlock::iterator i = --b->end();
+        errs() << "BasicBlock (name=" << b->getName() << ") has "<< b->size() << " instructions.\n";	
+        errs() << "Function (name=" << i->getName() << ")\n" ;	
+      }
+	
 }
 
 bool Liveness::runOnFunction(Function &F) {
+	errs() << "bla0\n";
     computeBBDefUse(F);
     computeBBInOut(F);
     computeIInOut(F);
-	return false;
+    return false;
 }
 
 char Liveness::ID = 0;
