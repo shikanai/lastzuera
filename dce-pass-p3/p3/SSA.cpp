e59e640ca6f939bd5dff676b871dab44839422dd
felipear <feliperosasd@gmail.com>
2015-05-27 16:49:30 -0300

    Começando... nada de mais

--- dce-pass-p3/p3/SSA.cpp
+++ dce-pass-p3/p3/SSA.cpp
@@ -8,7 +8,18 @@ bool SSA::runOnFunction(Function &F){
 	//Preciso encontrar uma função que pega o ultimo bloco.
 	//for (Function::reverse_iterator i = F.rbegin(); i != F.rend(); ++i ) { 
 		//
-	//} 
+	//}
+	
+	Function::iterator i = F.end();
+	while (i != F.begin())
+	{
+		 --i;
+		/* Aqui faco o que preciso */
+		for (BasicBlock::reverse_iterator bi = i->rbegin(), be = i->rend(); bi != be; ++bi){
+			
+		}
+
+	}
 }
 
 
@@ -17,4 +28,13 @@ bool SSA::runOnFunction(Function &F){
 
 char SSA::ID = 0;
 
-RegisterPass<SSA> X("ssa", "DCE para SSA", false, false);
+static RegisterPass<SSA> X("ssa", "DCE para SSA", false, false);
+
+/*
+ * Refs:
+ * 
+ * http://stackoverflow.com/questions/3610933/iterating-c-vector-from-the-end-to-the-begin
+ * 
+ * http://aviral.lab.asu.edu/?p=1699
+ * */
+
